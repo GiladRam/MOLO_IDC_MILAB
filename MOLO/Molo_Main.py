6838e3673dbcee589579c6c78a052a8942b4e6ad
Gilad Ram <giladram@gmail.com>
2018-06-09 20:39:35 +0300

    add config file and reboot system interval

--- MOLO/Molo_Main.py
+++ MOLO/Molo_Main.py
@@ -1,6 +1,8 @@
 from wifi import Cell, Scheme
 import subprocess as sp
-
+from WhoIsHere import WhoIsHere
+import configparser
+import os
 
 # airmon-ng variables
 AIRMON_NG = 'airmon-ng'
@@ -11,31 +13,52 @@ STOP_MONITOR_COMMAND = '%s stop %s' %(AIRMON_NG, MON_INTERFACE)
 
 # linux commands
 IFCONFIG = 'ifconfig'
+REBOOT_COMMAND = "shutdown -r +600"
 
-# Wifi AP dictionary
-WIFI_DIC = {'GD':'****', 'IDC-Wireless':'***', 'MiLab':'****'}
+configPath = os.path.join("root/MOLO/config", "config.ini")
+SECTION_KNOWN_WIFI = 'Known WIFI'
 
+config = configparser.ConfigParser()
+config.optionxform = str
+config.read(configPath)
+WIFI_DIC = dict()
 
-output = sp.Popen([AIRMON_NG, 'start', BASE_MON_INTERFACE], stdout=sp.PIPE)
-stream = output.communicate()[0]
-print output.returncode
-
-output = sp.Popen(IFCONFIG, stdout=sp.PIPE)
-
-while True:
-  line = output.stdout.readline()
-  if line != '':
-    print line.rstrip()
-  else:
-    break
-stream = output.communicate()[0]
-print output.returncode
+def set_all_known_wifis():
+    for wifi in config[SECTION_KNOWN_WIFI]:
+        WIFI_DIC[str(wifi)] = config[SECTION_KNOWN_WIFI][wifi]
+        if WIFI_DIC[str(wifi)] == '':
+            WIFI_DIC[str(wifi)] = None
 
 
-list_of_cells = Cell.all('wlan0')
-for cell in list_of_cells:
-    if cell.ssid in WIFI_DIC:
-        scheme = Scheme.for_cell('wlan0', cell.ssid, cell, WIFI_DIC[cell.ssid])
-        scheme.activate()
+def main():
+    output = sp.Popen([AIRMON_NG, 'start', BASE_MON_INTERFACE], stdout=sp.PIPE)
+    stream = output.communicate()[0]
+    print output.returncode
+    output = sp.Popen(IFCONFIG, stdout=sp.PIPE)
+    while True:
+        line = output.stdout.readline()
+        if line != '':
+            print line.rstrip()
+        else:
+            break
+    stream = output.communicate()[0]
+    print output.returncode
+    list_of_cells = Cell.all('wlan0')
+    for cell in list_of_cells:
+        if cell.ssid in WIFI_DIC:
+            print 'in!!!'
+            scheme = Scheme.for_cell('wlan0', cell.ssid, cell, WIFI_DIC[cell.ssid])
+            try:
+                scheme.save()
+            except:
+                print "saved scheme"
+            scheme.activate()
 
-import WhoIsHere
+
+if __name__ == '__main__':
+
+    set_all_known_wifis()
+    main()
+    os.system(REBOOT_COMMAND)
+    find = WhoIsHere.WhoIsHere()
+    find.run()
